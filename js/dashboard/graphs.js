08ce5b5ac16a67404a7738e1f6f05d8d0ba84175
Kayquedias <kayquedias204@gmail.com>
2024-12-10 12:31:28 -0300

    feat: make the logic to set the correct percentual values to graphs

--- js/dashboard/graphs.js
+++ js/dashboard/graphs.js
@@ -1,7 +1,7 @@
+import { getProductsStock, getProductsTransferred } from '../db.js'
+
 document.addEventListener('DOMContentLoaded', () => {
   const { isSameMonth } = window.dateFns
-
-  const productsStock = JSON.parse(localStorage.getItem('products_stock')) || []
 
   const buildPieChart = (data, element) => {
     const config = {
@@ -16,29 +16,57 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   const transferredQuantityChart = () => {
+    const productsTransferred = getProductsTransferred()
+
+    const currentMonthTransferredQuantity = productsTransferred.reduce((acc, product) => {
+      const sameMonth = isSameMonth(product.createdAt, new Date())
+      if (!sameMonth) return
+
+      return product.quantityTransferred + acc
+    }, 0) || 0
+
+    const lastMonthTransferredQuantity = productsTransferred.reduce((acc, product) => {
+      const sameMonth = isSameMonth(product.createdAt, new Date())
+      if (sameMonth) return
+
+      return product.quantityTransferred + acc
+    }, 0) || 0
+
+    const percentageDifference = calculateMonthPercentual(lastMonthTransferredQuantity, currentMonthTransferredQuantity)
+
+    // Set percentual value to html element
+    const percentualElement = document.querySelector('#transferred-percentual')
+    if (percentageDifference > 0) {
+      percentualElement.classList.add('text-green-500')
+      percentualElement.textContent = `+${percentageDifference}%`
+    } else {
+      percentualElement.classList.add('text-red-500')
+      percentualElement.textContent = `-${percentageDifference}%`
+    }
+
     const data = {
       labels: [
-        'Red',
-        'Blue',
-        'Yellow'
+        'Current month',
+        'Last month',
       ],
       datasets: [{
-        label: 'My First Dataset',
-        data: [300, 50, 100],
+        label: 'Total',
+        data: [currentMonthTransferredQuantity, lastMonthTransferredQuantity],
         backgroundColor: [
           'rgb(255, 99, 132)',
           'rgb(54, 162, 235)',
-          'rgb(255, 205, 86)'
         ],
         hoverOffset: 4
       }]
     };
-    const chartContainer = document.getElementById('transferred-quantity')
 
+    const chartContainer = document.getElementById('transferred-quantity')
     buildPieChart(data, chartContainer)
   }
 
   const receivedQuantityChart = () => {
+    const productsStock = getProductsStock()
+
     const lastMonthReceivedQuantity = productsStock.reduce((acc, product) => {
       const sameMonth = isSameMonth(product.createdAt, new Date())
       if (sameMonth) return
@@ -52,6 +80,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
       return product.quantityAdded + acc
     }, 0)
+
+    const percentageDifference = calculateMonthPercentual(lastMonthReceivedQuantity, currentMonthReceivedQuantity)
+
+    // Set percentual value to html element
+    const percentualElement = document.querySelector('#received-percentual')
+    if (percentageDifference > 0) {
+      percentualElement.classList.add('text-green-500')
+      percentualElement.textContent = `+${percentageDifference}%`
+    } else {
+      percentualElement.classList.add('text-red-500')
+      percentualElement.textContent = `-${percentageDifference}%`
+    }
 
     const data = {
       labels: [
@@ -68,9 +108,24 @@ document.addEventListener('DOMContentLoaded', () => {
         hoverOffset: 4
       }]
     };
-    const chartContainer = document.getElementById('received-quantity')
 
+    const chartContainer = document.getElementById('received-quantity')
     buildPieChart(data, chartContainer)
+  }
+
+
+  const calculateMonthPercentual = (lastMonthQuantity = 0, currentMonthQuantity = 0) => {
+    // Avoid division by zero if last month's quantity is 0
+    const percentageDifference =
+    lastMonthQuantity > 0
+      ? ((currentMonthQuantity - lastMonthQuantity) /
+          lastMonthQuantity) *
+        100
+      : currentMonthQuantity > 0
+      ? 100 // If last month's quantity was 0 and current month has transfers
+      : 0; // No transfers in either month
+
+      return percentageDifference
   }
 
   transferredQuantityChart()
