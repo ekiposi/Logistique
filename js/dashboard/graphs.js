578ec1b753e6b7df7ca616ee7c18260f88771ada
Kayquedias <kayquedias204@gmail.com>
2024-12-09 23:40:01 -0300

    feat: add received quantity data to the graph

--- js/dashboard/graphs.js
+++ js/dashboard/graphs.js
@@ -1,60 +1,78 @@
-const buildPieChart = (data, element) => {
-  const config = {
-    type: 'pie',
-    data: data,
-  };
+document.addEventListener('DOMContentLoaded', () => {
+  const { isSameMonth } = window.dateFns
 
-  new Chart(
-    element,
-    config
-  )
-}
+  const productsStock = JSON.parse(localStorage.getItem('products_stock')) || []
 
-const transferredQuantityChart = () => {
-  const data = {
-    labels: [
-      'Red',
-      'Blue',
-      'Yellow'
-    ],
-    datasets: [{
-      label: 'My First Dataset',
-      data: [300, 50, 100],
-      backgroundColor: [
-        'rgb(255, 99, 132)',
-        'rgb(54, 162, 235)',
-        'rgb(255, 205, 86)'
+  const buildPieChart = (data, element) => {
+    const config = {
+      type: 'pie',
+      data: data,
+    };
+
+    new Chart(
+      element,
+      config
+    )
+  }
+
+  const transferredQuantityChart = () => {
+    const data = {
+      labels: [
+        'Red',
+        'Blue',
+        'Yellow'
       ],
-      hoverOffset: 4
-    }]
-  };
-  const chartContainer = document.getElementById('transferred-quantity')
+      datasets: [{
+        label: 'My First Dataset',
+        data: [300, 50, 100],
+        backgroundColor: [
+          'rgb(255, 99, 132)',
+          'rgb(54, 162, 235)',
+          'rgb(255, 205, 86)'
+        ],
+        hoverOffset: 4
+      }]
+    };
+    const chartContainer = document.getElementById('transferred-quantity')
 
-  buildPieChart(data, chartContainer)
-}
+    buildPieChart(data, chartContainer)
+  }
 
-const receivedQuantityChart = () => {
-  const data = {
-    labels: [
-      'Red',
-      'Blue',
-      'Yellow'
-    ],
-    datasets: [{
-      label: 'My First Dataset',
-      data: [300, 50, 100],
-      backgroundColor: [
-        'rgb(255, 99, 132)',
-        'rgb(54, 162, 235)',
-        'rgb(255, 205, 86)'
+  const receivedQuantityChart = () => {
+    const lastMonthReceivedQuantity = productsStock.reduce((acc, product) => {
+      const sameMonth = isSameMonth(product.createdAt, new Date())
+      if (sameMonth) return
+
+      return product.quantityAdded + acc
+    }, 0) || 0
+
+    const currentMonthReceivedQuantity = productsStock.reduce((acc, product) => {
+      const sameMonth = isSameMonth(product.createdAt, new Date())
+      if (!sameMonth) return
+
+      return product.quantityAdded + acc
+    }, 0)
+
+    const data = {
+      labels: [
+        'Current month',
+        'Last month',
       ],
-      hoverOffset: 4
-    }]
-  };
-  const chartContainer = document.getElementById('received-quantity')
+      datasets: [{
+        label: 'Total',
+        data: [currentMonthReceivedQuantity, lastMonthReceivedQuantity],
+        backgroundColor: [
+          'rgb(255, 99, 132)',
+          'rgb(54, 162, 235)',
+        ],
+        hoverOffset: 4
+      }]
+    };
+    const chartContainer = document.getElementById('received-quantity')
 
-  buildPieChart(data, chartContainer)
-}
+    buildPieChart(data, chartContainer)
+  }
 
-transferredQuantityChart()
-receivedQuantityChart()
+  transferredQuantityChart()
+  receivedQuantityChart()
+})
