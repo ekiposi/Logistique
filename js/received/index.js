c69abc497cb4f92251ddff41a6e3858fd5f759f1
Kayquedias <kayquedias204@gmail.com>
2024-12-12 00:17:56 -0300

    feat: implement list products on product input

--- js/received/index.js
+++ js/received/index.js
@@ -1,8 +1,12 @@
-import { addProductStock, updateMedications } from '../db.js'
+import { addProductStock, updateMedications, getEquipments, getDevices, getDataFromStorage } from '../db.js'
 
 document.addEventListener('DOMContentLoaded', () => {
   const medications = JSON.parse(localStorage.getItem('medications')) || []
   
+  const dataListElement = document.querySelector('#product-list')
+
+  let productType = 'medications'
+
   const addProductReceived = (e) => {
     e.preventDefault()
 
@@ -10,6 +14,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const [type, productName, newQuantity, supplierName] = fields.map((fieldId) => {
       return document.querySelector(fieldId).value
     })
+
+    console.log(type, productName, newQuantity, supplierName)
 
     if (!type || !productName || !newQuantity || !supplierName) {
       window.alert('Invalid values in some fields')
@@ -54,6 +60,39 @@ document.addEventListener('DOMContentLoaded', () => {
     updateMedications(updatedProductsList)
     return true
   }
+
+  const removeProducts = () => {
+    dataListElement.innerHTML = ''
+  }
+
+  const listProducts = (e) => {
+    removeProducts()
+
+    const products = getDataFromStorage(productType)
+
+    products.map((product) => {
+      const optionElement = document.createElement('option')
+      optionElement.value = product.name
+      optionElement.textContent = product.name
+
+      dataListElement.appendChild(optionElement)
+    })
+  }
+
+  const setProductType = () => {
+    const productTypeValue = document.querySelector('#product-type').value
+    if(!productTypeValue) return
+
+    productType = productTypeValue
+
+    listProducts()
+  }
+
+  const searchProductInput = document.querySelector('#product-list')
+  searchProductInput.addEventListener('change', listProducts)
+
+  const typeInput = document.querySelector('#product-type')
+  typeInput.addEventListener('change', setProductType)
 
   const addReceivedQuantity = document.querySelector('#add-quantity-form')
   addReceivedQuantity.addEventListener('submit', addProductReceived)
