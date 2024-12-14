bc6fa3605c68f26e615b786f39ac9ec7582eb6a3
Joel Holanda <joelholanda45@gmail.com>
2024-12-14 01:32:41 -0300

    add list in sent.html

--- js/sent/index.js
+++ js/sent/index.js
@@ -1,6 +1,11 @@
-import { addProductTransferred, updateMedications, getMedications } from '../db.js'
+import { addProductTransferred, updateMedications, getMedications, getDataFromStorage, getProductsTransferred } from '../db.js'
+import { renderTable } from './table.js'
+const { isBefore } = window.dateFns
 
 document.addEventListener('DOMContentLoaded', () => {
+
+  const productSelect = document.querySelector('#product')
+
   const createTransfer = (e) => {
     e.preventDefault()
 
@@ -63,6 +68,112 @@ document.addEventListener('DOMContentLoaded', () => {
     return true
   }
 
+  let productType = 'medications'
+  let products = getDataFromStorage(productType)
+
+  const onProductTypeChange = () => {
+    const productTypeValue = document.querySelector('#product-type').value
+    if(!productTypeValue) return
+
+    // Update global productType global var
+    productType = productTypeValue
+    products = getDataFromStorage(productTypeValue)
+
+
+    const currentStockElement = document.querySelector('#current-stock')
+    currentStockElement.textContent = 
+
+    listProducts()
+  }
+
+  const addProductReceived = (e) => {
+    e.preventDefault()
+
+    const fields = ['#product-type', '#product', '#data-tranferenc', '#transfer-quantity', '#transfer-reason' ]
+    const [type, productName, tranferenceDate, quantityTransferred, reason] = fields.map((fieldId) => {
+      return document.querySelector(fieldId).value
+    })
+
+    if (!type || !productName || !quantityTransferred || !tranferenceDate || !reason) {
+      window.alert('Invalid values in some fields')
+      return
+    }
+  
+    if (!selectedProduct) {
+      window.alert('Product not found!')
+      return 
+    }
+
+    const data = {
+      id: Math.floor(Math.random() * 1000000),
+      type,
+      productId: productName,
+      quantityTransferred: Number(quantityTransferred),
+      newQuantity: Number(quantityTransferred) + selectedProduct.quantity,
+      reason,
+      previousQuantity: selectedProduct.quantity,
+      tranferenceDate,
+      createdAt: new Date(),
+    }
+
+    // Check if product is expired
+    const isExpired = isBefore(new Date(selectedProduct.expirationDate), new Date())
+    if(isExpired) {
+      window.alert('Selected product is expired.')
+      return
+    }
+
+    // Update product quantity
+    const isConcluded = updateProductQuantity(productName, Number(quantityTransferred))
+    if (!isConcluded) return
+
+    // Add to local storage
+    addProductTransferred(data)
+
+    const updatedStock = getProductsTransferred()
+    console.log(updatedStock)
+    renderTable(updatedStock)
+    window.alert('Transfer created!')
+  }
+
+  const listProducts = () => {
+    // removeProductsList()
+
+    products.map((product) => {
+      const optionElement = document.createElement('option')
+      optionElement.value = product.name
+      optionElement.textContent = product.name
+
+      productSelect.append(optionElement)
+    })
+  }
+  let selectedProduct = {}
+  const onProductSelect = (e) => {
+    const currentStockElement = document.querySelector('#current-stock')
+
+    const productName = e.target.value
+    if (!productName) {
+      selectedProduct = {}
+      currentStockElement.value = 0
+      return
+    }
+
+    selectedProduct = products.find((prod) => prod.name === productName)
+
+    currentStockElement.value = selectedProduct.quantity
+  }
+
+  listProducts()
+
+  productSelect.addEventListener('change', onProductSelect)
+
+  const typeInput = document.querySelector('#report-product-type')
+  typeInput.addEventListener('change', (e) => onProductTypeChange(e))
+
   const transferQuantityForm = document.querySelector('#transfer-quantity-form')
   transferQuantityForm.addEventListener('submit', createTransfer)
+
+
+  const addReceivedQuantity = document.querySelector('#transfer-quantity-form')
+  addReceivedQuantity.addEventListener('submit', addProductReceived)
 })
---
+++ js/sent/table.js
@@ -0,0 +1,76 @@
+import { formatDate, capitalize } from '../utils.js'
+import { getDataFromStorage, getProductsTransferred } from '../db.js'
+const { isBefore, isAfter ,isSameDay, endOfDay, startOfDay, addMinutes } = window.dateFns
+
+const productsReceivedTableBody = document.querySelector('#products-received-table tbody');
+const startDateInput = document.getElementById('start-date');
+const endDateInput = document.getElementById('end-date');
+const productTypeFilter = document.getElementById('report-product-type');
+
+const products = getDataFromStorage(productTypeFilter.value)
+console.log(productTypeFilter)
+export const renderTable = (data) => {
+  const productsTransferred = data || getProductsTransferred()
+  productsReceivedTableBody.innerHTML = ''
+
+  if (productsTransferred.length === 0) {
+    productsReceivedTableBody.innerHTML = '<tr><td colspan="11" style="text-align: center;">Aucune donnée disponible</td></tr>';
+    return;
+  }
+  console.log(products)
+
+  productsTransferred.forEach((tranferedProduct) => {
+    const product = products.find((item) => item.name === tranferedProduct.productId)
+    if(!product) return
+    const row = document.createElement('tr');
+    row.innerHTML = `
+      <td>${product.name}</td>
+      <td>${product.quantity}</td>
+      <td>${formatDate(tranferedProduct.createdAt)}</td>
+      <td>Fonction</td>
+      <td>${capitalize(tranferedProduct.type)}</td>
+      <td>${product.quantity < 4 ? 'Oui' : 'Non'}</td>
+      <td>${tranferedProduct.reason}</td>
+      <td>${tranferedProduct.previousQuantity}</td>
+      <td>${tranferedProduct.quantityTransferred}</td>
+      <td>${tranferedProduct.newQuantity}</td>
+      <td class="flex gap-2.5 border-none">
+        <img src="../../images/icons/edit.png" alt="" class="w-7 h-7 cursor-pointer" />
+        <img src="../../images/icons/trash.png" alt="" class="w-7 h-7 cursor-pointer" />
+      </td>
+    `;
+    productsReceivedTableBody.appendChild(row);
+  });
+
+  const filterData = () => {
+    const startDate = startDateInput.value ? startOfDay(startDateInput.value) : null;
+    const endDate = endDateInput.value ? addMinutes(endOfDay(endDateInput.value), 1) : null;
+    const filter = productTypeFilter.value;
+    console.log(filter)
+
+    const productsTransferred = getProductsTransferred()
+    const filteredData = productsTransferred.filter((tranferedProduct) => {
+        const receivedDate = startOfDay(new Date(tranferedProduct.receptionDate));
+
+        // Date filters
+        const isWithinStartDate = startDate ? isAfter(receivedDate, startDate) || isSameDay(receivedDate, startDate) : true;
+        const isWithinEndDate = endDate ? isBefore(receivedDate, endDate) || isSameDay(receivedDate, endDate) : true;
+
+        // Type filters
+        const matchesFilter = 
+          filter === 'medications' ? tranferedProduct.type === 'medications' :
+          filter === 'equipments' ? tranferedProduct.type === 'equipments' :
+          filter === 'devices' ? tranferedProduct.type === 'devices' :
+          true;
+
+        return isWithinStartDate && isWithinEndDate && matchesFilter;
+    });
+
+    renderTable(filteredData);
+  };
+
+  const filterButton = document.querySelector('#filter-button')
+  filterButton.addEventListener('click', filterData)
+}
+
+renderTable()
